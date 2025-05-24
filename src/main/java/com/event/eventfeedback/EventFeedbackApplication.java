b4a4435dab0c81ac3e4e886025206d0771a92ec5
IT124103093 <it24103093@my.sliit.lk>
2025-05-24 16:49:16 +0530

    Added all the components

--- src/main/java/com/event/eventfeedback/EventFeedbackApplication.java
+++ src/main/java/com/event/eventfeedback/EventFeedbackApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventFeedbackApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EventFeedbackApplication.class, args);
     }
+
 }
