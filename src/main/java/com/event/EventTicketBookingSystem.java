85940cb38eb3e16cdfb21366d6b9d8515821844e
IT24102157 <it24102157@my.sliit.lk>
2025-05-24 18:27:50 +0530

    edited version

--- src/main/java/com/event/EventTicketBookingSystem.java
+++ src/main/java/com/event/EventTicketBookingSystem.java
@@ -3,10 +3,9 @@ package com.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ticketbooking")
 public class EventTicketBookingSystem {
-
     public static void main(String[] args) {
-        SpringApplication.run(EventTicketBookingSystem.class, args);
+        SpringApplication.run(com.event.EventTicketBookingSystem.class, args);
     }
 }
